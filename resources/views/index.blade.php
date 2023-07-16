a603e1c7c0cde49772141b439c1b88ead1aa48ff
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-07-16 08:47:34 +0800

    Progress: Progress: Finish About Section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,7 +70,7 @@
                             </div>
                         </div>
                         <div class="col-lg-5 col-xl-6 col-xxl-5 ps-lg-5 ps-xxl-0 d-none d-lg-flex justify-content-end">
-                            <img src="{{ asset('assets/img/banner/hero-banner.svg') }}" class="img-fluid hero-banner"
+                            <img src="{{ asset('assets/img/banner/hero-banner.svg') }}" class="img-fluid Hero Banner"
                                 alt="Hero Banner">
                         </div>
                     </div>
@@ -173,6 +173,125 @@
                 </div>
             </section>
             {{-- END STEP SECTION --}}
+
+
+            {{-- STORIES SECTION --}}
+            <section class="stories section-gap" id="stories">
+                <div class="row">
+                    <div class="col-lg-7 col-xl-6 order-2 order-lg-1">
+                        <div class="wrapper">
+                            <div class="swiper mySwiperStories">
+                                <div class="swiper-wrapper">
+                                    <div class="swiper-slide">
+                                        <div class="card-default">
+                                            <img src="{{ asset('assets/img/stories/stories-1.svg') }}"
+                                                class="img-fluid rounded-3" alt="Stories Image Profile">
+                                            <h6 style="margin: 18px 0 2px;">Emma Johnson</h6>
+                                            <p class="paragraph-primary" style="margin-bottom: 8px;">Singing Category
+                                                Winner</p>
+                                            <p class="paragraph-small">Join Our Talent Contest and Showcase Your
+                                                Skills!"
+                                                Participate in our talent contest by registering online and get ready to
+                                                showcase your unique talents to the world.</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="swiper-slide">
+                                        <div class="card-default">
+                                            <img src="{{ asset('assets/img/stories/stories-2.svg') }}"
+                                                class="img-fluid rounded-3" alt="Stories Image Profile">
+                                            <h6 style="margin: 18px 0 2px;">David Thompson</h6>
+                                            <p class="paragraph-primary" style="margin-bottom: 8px;">Dance Category
+                                                Winner</p>
+                                            <p class="paragraph-small">David's dynamic and innovative dance moves won
+                                                the hearts of the judges and the audience alike.</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="swiper-slide">
+                                        <div class="card-default">
+                                            <img src="{{ asset('assets/img/stories/stories-1.svg') }}"
+                                                class="img-fluid rounded-3" alt="Stories Image Profile">
+                                            <h6 style="margin: 18px 0 2px;">Emma Johnson</h6>
+                                            <p class="paragraph-primary" style="margin-bottom: 8px;">Singing Category
+                                                Winner</p>
+                                            <p class="paragraph-small">Join Our Talent Contest and Showcase Your
+                                                Skills!"
+                                                Participate in our talent contest by registering online and get ready to
+                                                showcase your unique talents to the world.</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="swiper-slide">
+                                        <div class="card-default">
+                                            <img src="{{ asset('assets/img/stories/stories-2.svg') }}"
+                                                class="img-fluid rounded-3" alt="Stories Image Profile">
+                                            <h6 style="margin: 18px 0 2px;">David Thompson</h6>
+                                            <p class="paragraph-primary" style="margin-bottom: 8px;">Dance Category
+                                                Winner</p>
+                                            <p class="paragraph-small">David's dynamic and innovative dance moves won
+                                                the hearts of the judges and the audience alike.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-pagination-stories"></div>
+                        </div>
+                    </div>
+
+                    <div class="offset-xl-1 col-lg-5 mb-5 mb-lg-0 order-1 order-lg-2">
+                        <span class="subtitle d-inline-block" style="margin-bottom: 14px !important;">Inspiring Paths
+                            to Success</span>
+                        <h2 class="title" style="margin-bottom: 18px !important; padding-right: 24px;">Stories of
+                            Triumph and Inspiring Achievements</h2>
+                        <p class="paragraph">Get inspired by the extraordinary journeys of our past winners. Through
+                            their talent, resilience, and unwavering commitment, they have not only won the contest but
+                            have also become role models for aspiring talents around the world.</p>
+                    </div>
+                </div>
+            </section>
+            {{-- END STORIES SECTION --}}
+
+
+            {{-- ABOUT SECTION --}}
+            <section class="about d-flex align-items-center section-gap pt-5 section-gap-top" id="about">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 col-xl-6 col-xxl-5 pe-lg-5 pe-xxl-0 d-none d-lg-flex justify-content-start">
+                        <img src="{{ asset('assets/img/banner/about-banner.svg') }}" class="img-fluid About Banner"
+                            alt="Hero Banner">
+                    </div>
+                    <div class="col-lg-7 col-xl-6 col-xxl-7 ps-xxl-5 ps-xxl-0">
+                        <span class="subtitle d-inline-block" style="margin-bottom: 14px !important;">Unleash Your
+                            Creativity, Inspire the World</span>
+                        <h2 class="title" style="padding-right: 24px;">Unleashing the
+                            Power of Talent, A Platform for Dreamers to Shine and Inspire</h2>
+                        <div class="wrapper-about d-flex gap-2" style="margin: 18px 0 20px !important;">
+                            <button class="button-about active">Introduce</button>
+                            <button class="button-about">Vision & Mission</button>
+                            <button class="button-about">Our History</button>
+                        </div>
+                        <div class="wrapper-paragraph d-flex flex-column gap-2"
+                            style="margin-bottom: 36px !important;">
+                            <p class="paragraph">At our talent contest, we are committed to unleashing the power of
+                                talent and providing a platform for dreamers to showcase their unique abilities. We
+                                believe that everyone deserves a chance to be heard, seen, and celebrated for their
+                                extraordinary gifts.</p>
+                            <p class="paragraph">With a vibrant community of artists, mentors, and enthusiasts, we
+                                foster an environment that sparks creativity, fosters growth, and inspires others to
+                                pursue their passions. Join us on this incredible journey of talent discovery, where
+                                dreams become reality and stars are born.</p>
+                        </div>
+                        <div class="wrapper-button d-flex gap-2 align-items-md-center">
+                            <a href="#" class="button-default">Contact Us</a>
+                            <a href="#" class="button-third d-flex align-items-center gap-2">
+                                Check Criteria
+                                <div class="arrow-icon"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {{-- END ABOUT SECTION --}}
         </div>
     </main>
     {{-- SCRIPT JS --}}
@@ -181,10 +300,9 @@
     <script src="{{ asset('assets/js/script.js') }}"></script>
 
     <script>
-        var swiper = new Swiper(".mySwiper", {
+        var swiper = new Swiper(".mySwiperStories", {
             slidesPerView: 1,
             spaceBetween: 20,
-            loop: true,
             breakpoints: {
                 640: {
                     slidesPerView: 2,
@@ -192,7 +310,7 @@
                 }
             },
             pagination: {
-                el: ".swiper-pagination",
+                el: ".swiper-pagination-stories",
                 clickable: true,
             },
         });
